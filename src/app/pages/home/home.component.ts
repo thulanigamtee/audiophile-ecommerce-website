844ad2a91063b452e860ea573d91f5728f418166
thulanigamtee <thulanigamtee@gmail.com>
2024-11-14 03:53:57 +0200

    update speaker and earphone image data types

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/shared/button/button.component';
 import { ProductCardComponent } from '../../components/shared/product-card/product-card.component';
 import { PicturesComponent } from '../../components/shared/pictures/pictures.component';
+import { Image } from '../../models/image.interface';
 
 @Component({
   selector: 'app-home',
@@ -9,27 +10,15 @@ import { PicturesComponent } from '../../components/shared/pictures/pictures.com
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  speakerImages: {
-    desktop: string;
-    tablet: string;
-    mobile: string;
-    default: string;
-  } = {
+  speakerImage: Image = {
     desktop: '/assets/home/desktop/image-speaker-zx9.png',
     tablet: '/assets/home/tablet/image-speaker-zx9.png',
     mobile: '/assets/home/mobile/image-speaker-zx9.png',
-    default: '/assets/home/mobile/image-speaker-zx9.png',
   };
 
-  earphonesImages: {
-    desktop: string;
-    tablet: string;
-    mobile: string;
-    default: string;
-  } = {
+  earphonesImage: Image = {
     desktop: '/assets/home/desktop/image-earphones-yx1.jpg',
     tablet: '/assets/home/tablet/image-earphones-yx1.jpg',
     mobile: '/assets/home/mobile/image-earphones-yx1.jpg',
-    default: '/assets/home/mobile/image-earphones-yx1.jpg',
   };
 }
