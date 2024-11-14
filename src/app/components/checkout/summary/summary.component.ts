e9a897ea4250330a4de7b3cf9e520abec4bb9898
thulanigamtee <thulanigamtee@gmail.com>
2024-11-14 09:28:07 +0200

    add function to show modal

--- src/app/components/checkout/summary/summary.component.ts
+++ src/app/components/checkout/summary/summary.component.ts
@@ -4,6 +4,8 @@ import { CartService } from '../../../services/cart.service';
 import { CartItem } from '../../../models/cartItem.interface';
 import { Subject, takeUntil } from 'rxjs';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { ModalService } from '../../../services/modal.service';
+import { OverlayService } from '../../../services/overlay.service';
 
 @Component({
   selector: 'app-summary',
@@ -15,7 +17,11 @@ export class SummaryComponent {
   totalPrice: number = 0;
   private destroy$ = new Subject<void>();
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private modalService: ModalService,
+    private overlayService: OverlayService
+  ) {}
 
   ngOnInit() {
     this.cartService.cartSubject$.pipe(takeUntil(this.destroy$)).subscribe({
@@ -32,6 +38,11 @@ export class SummaryComponent {
     { name: 'vat (included)', amount: 1079 },
     { name: 'grand total', amount: this.totalPrice + 50 },
   ];
+
+  showModal() {
+    this.modalService.modal = true;
+    this.overlayService.overlay = true;
+  }
 
   ngOnDestroy() {
     this.destroy$.next();
