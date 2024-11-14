f077b4b9179c1a9f6d52e848f4978d5708a3b6ba
thulanigamtee <thulanigamtee@gmail.com>
2024-11-14 03:16:17 +0200

    update summary information

--- src/app/components/checkout/summary/summary.component.ts
+++ src/app/components/checkout/summary/summary.component.ts
@@ -14,11 +14,11 @@ import { OverlayService } from '../../../services/overlay.service';
 })
 export class SummaryComponent {
   summaryItems: CartItem[] = [];
-  totalPrice: number = 0;
+  summaryInformation: { name: string; amount: number }[] = [];
   private destroy$ = new Subject<void>();
 
   constructor(
-    private cartService: CartService,
+    public cartService: CartService,
     private modalService: ModalService,
     private overlayService: OverlayService
   ) {}
@@ -27,17 +27,15 @@ export class SummaryComponent {
     this.cartService.cart$.pipe(takeUntil(this.destroy$)).subscribe({
       next: (items) => {
         this.summaryItems = items;
-        this.totalPrice = this.cartService.totalPrice;
+        this.summaryInformation = [
+          { name: 'total', amount: this.cartService.totalPrice },
+          { name: 'shipping', amount: 50 },
+          { name: 'vat (included)', amount: 1079 },
+          { name: 'grand total', amount: this.cartService.totalPrice + 50 },
+        ];
       },
     });
   }
-
-  summaryInformation = [
-    { name: 'total', amount: '' },
-    { name: 'shipping', amount: 50 },
-    { name: 'vat (included)', amount: 1079 },
-    { name: 'grand total', amount: this.totalPrice + 50 },
-  ];
 
   showModal() {
     this.modalService.modalState = true;
