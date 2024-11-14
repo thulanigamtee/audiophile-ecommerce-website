fa30563a00990a31911c9672712f865c4904c728
thulanigamtee <thulanigamtee@gmail.com>
2024-11-14 03:15:30 +0200

    lazy load pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,46 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { HeadphonesComponent } from './pages/headphones/headphones.component';
-import { SpeakersComponent } from './pages/speakers/speakers.component';
-import { EarphonesComponent } from './pages/earphones/earphones.component';
-import { DetailsComponent } from './pages/details/details.component';
-import { CheckoutComponent } from './pages/checkout/checkout.component';
 
 export const routes: Routes = [
-  { component: HomeComponent, path: '' },
-  { component: HeadphonesComponent, path: 'headphones' },
-  { component: SpeakersComponent, path: 'speakers' },
-  { component: EarphonesComponent, path: 'earphones' },
-  { component: DetailsComponent, path: ':category/details/:id' },
-  { component: CheckoutComponent, path: 'checkout' },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then(
+        (component) => component.HomeComponent
+      ),
+  },
+  {
+    path: 'headphones',
+    loadComponent: () =>
+      import('./pages/headphones/headphones.component').then(
+        (component) => component.HeadphonesComponent
+      ),
+  },
+  {
+    path: 'speakers',
+    loadComponent: () =>
+      import('./pages/speakers/speakers.component').then(
+        (component) => component.SpeakersComponent
+      ),
+  },
+  {
+    path: 'earphones',
+    loadComponent: () =>
+      import('./pages/earphones/earphones.component').then(
+        (component) => component.EarphonesComponent
+      ),
+  },
+  {
+    path: ':category/details/:id',
+    loadComponent: () =>
+      import('./pages/details/details.component').then(
+        (component) => component.DetailsComponent
+      ),
+  },
+  {
+    path: 'checkout',
+    loadComponent: () =>
+      import('./pages/checkout/checkout.component').then(
+        (component) => component.CheckoutComponent
+      ),
+  },
 ];
