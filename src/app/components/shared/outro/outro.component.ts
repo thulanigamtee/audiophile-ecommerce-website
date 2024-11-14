77c7bbff0ee174cfcdbdbf7889760a5feda2d9b0
thulanigamtee <thulanigamtee@gmail.com>
2024-11-14 03:24:57 +0200

    update outro component

--- src/app/components/shared/outro/outro.component.ts
+++ src/app/components/shared/outro/outro.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PicturesComponent } from '../pictures/pictures.component';
+import { Image } from '../../../models/image.interface';
 
 @Component({
   selector: 'app-outro',
@@ -7,15 +8,9 @@ import { PicturesComponent } from '../pictures/pictures.component';
   templateUrl: './outro.component.html',
 })
 export class OutroComponent {
-  bestGearImages: {
-    desktop: string;
-    tablet: string;
-    mobile: string;
-    default: string;
-  } = {
+  bestGearImages: Image = {
     desktop: '/assets/shared/desktop/image-best-gear.jpg',
     tablet: '/assets/shared/tablet/image-best-gear.jpg',
     mobile: '/assets/shared/mobile/image-best-gear.jpg',
-    default: '/assets/shared/mobile/image-best-gear.jpg',
   };
 }
