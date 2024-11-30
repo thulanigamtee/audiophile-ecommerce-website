0c5336b0e4b4e404097755cfb639f0fb84cf1e95
thulanigamtee <thulanigamtee@gmail.com>
2024-11-30 08:54:15 +0200

    fix home path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { SpeakersComponent } from './pages/speakers/speakers.component';
 import { EarphonesComponent } from './pages/earphones/earphones.component';
 
 export const routes: Routes = [
-  { component: HomeComponent, path: '/' },
+  { component: HomeComponent, path: '' },
   { component: HeadphonesComponent, path: 'headphones' },
   { component: SpeakersComponent, path: 'speakers' },
   { component: EarphonesComponent, path: 'earphones' },
