28ed5786d22e714491b63dc9ca401a0e0663b977
thulanigamtee <thulanigamtee@gmail.com>
2024-11-30 21:02:07 +0200

    add outro and footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { OutroComponent } from './components/outro/outro.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    CommonModule,
+    OutroComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  isHomePage: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isHomePage = event.urlAfterRedirects === '/';
+      }
+    });
+  }
+}
