9a5493692ff85053118f72719a576980193a85ec
thulanigamtee <thulanigamtee@gmail.com>
2024-11-30 21:00:51 +0200

    add components to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonComponent, ProductCardComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
