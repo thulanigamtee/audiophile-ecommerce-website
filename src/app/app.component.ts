9a4c6a2a23a2ad4a781b9ed3b3a42e9c39159b46
thulanigamtee <thulanigamtee@gmail.com>
2024-12-01 13:58:39 +0200

    use route to select background class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,15 +17,27 @@ import { FooterComponent } from './components/footer/footer.component';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  isHomePage: boolean = false;
+  backgroundClass: string = '';
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.isHomePage = event.urlAfterRedirects === '/';
+        this.setBackgroundClass(event.urlAfterRedirects);
       }
     });
+  }
+
+  setBackgroundClass(url: string): void {
+    if (url === '/') {
+      this.backgroundClass = 'home-background';
+    } else if (url.startsWith('/headphones')) {
+      this.backgroundClass = 'headphones-background';
+    } else if (url.startsWith('/speakers')) {
+      this.backgroundClass = 'speakers-background';
+    } else if (url.startsWith('/earphones')) {
+      this.backgroundClass = 'earphones-background';
+    }
   }
 }
