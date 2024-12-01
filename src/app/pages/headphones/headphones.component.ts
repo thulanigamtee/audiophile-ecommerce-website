aec08d130e78cf6a3a2039d77b9e380b71397864
thulanigamtee <thulanigamtee@gmail.com>
2024-12-01 17:24:32 +0200

    subscribe to data service

--- src/app/pages/headphones/headphones.component.ts
+++ src/app/pages/headphones/headphones.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProductsComponent } from '../../components/products/products.component';
+import { DataService } from '../../services/data.service';
+import { Subject, takeUntil } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-headphones',
-  imports: [],
+  imports: [ProductsComponent, CommonModule],
   templateUrl: './headphones.component.html',
 })
-export class HeadphonesComponent {}
+export class HeadphonesComponent implements OnInit, OnDestroy {
+  headphonesData!: any;
+  private $destroy = new Subject<void>();
+
+  constructor(private dataService: DataService) {}
+
+  getHeadphonesData() {
+    this.dataService.data.pipe(takeUntil(this.$destroy)).subscribe({
+      next: (data) => {
+        this.headphonesData = data.filter(
+          (item: any) => item.category === 'headphones'
+        );
+      },
+    });
+  }
+
+  ngOnInit() {
+    this.getHeadphonesData();
+  }
+
+  ngOnDestroy() {
+    this.$destroy.next();
+    this.$destroy.complete();
+  }
+}
