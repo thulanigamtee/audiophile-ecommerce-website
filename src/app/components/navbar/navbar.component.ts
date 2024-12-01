ae8d3ee5a3aa105f8fbfc4e5b108ef37f5c1eabb
thulanigamtee <thulanigamtee@gmail.com>
2024-12-01 12:24:50 +0200

    import common module

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
