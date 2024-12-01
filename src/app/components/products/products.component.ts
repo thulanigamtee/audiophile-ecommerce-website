168ab3b20bb1c1b668ccb3b5fc82622703dddeca
thulanigamtee <thulanigamtee@gmail.com>
2024-12-01 13:58:56 +0200

    create products page

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  imports: [],
+  templateUrl: './products.component.html',
+})
+export class ProductsComponent {}
