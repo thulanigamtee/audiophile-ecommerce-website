628980b87e03ec0309105d5e0a4b07f52cbfc976
thulanigamtee <thulanigamtee@gmail.com>
2024-12-02 04:50:00 +0200

    implement picture component

--- src/app/components/outro/outro.component.ts
+++ src/app/components/outro/outro.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { PicturesComponent } from '../pictures/pictures.component';
 
 @Component({
   selector: 'app-outro',
-  imports: [],
+  imports: [PicturesComponent],
   templateUrl: './outro.component.html',
 })
-export class OutroComponent {}
+export class OutroComponent {
+  bestGearImages: {
+    desktopImage: string;
+    tabletImage: string;
+    mobileImage: string;
+    defaultImage: string;
+  } = {
+    desktopImage: '/assets/shared/desktop/image-best-gear.jpg',
+    tabletImage: '/assets/shared/tablet/image-best-gear.jpg',
+    mobileImage: '/assets/shared/mobile/image-best-gear.jpg',
+    defaultImage: '/assets/shared/mobile/image-best-gear.jpg',
+  };
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { PicturesComponent } from '../../components/pictures/pictures.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent, ProductCardComponent],
+  imports: [ButtonComponent, ProductCardComponent, PicturesComponent],
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  speakerImages: {
+    desktopImage: string;
+    tabletImage: string;
+    mobileImage: string;
+    defaultImage: string;
+  } = {
+    desktopImage: '/assets/home/desktop/image-speaker-zx9.png',
+    tabletImage: '/assets/home/tablet/image-speaker-zx9.png',
+    mobileImage: '/assets/home/mobile/image-speaker-zx9.png',
+    defaultImage: '/assets/home/mobile/image-speaker-zx9.png',
+  };
+
+  earphonesImages: {
+    desktopImage: string;
+    tabletImage: string;
+    mobileImage: string;
+    defaultImage: string;
+  } = {
+    desktopImage: '/assets/home/desktop/image-earphones-yx1.jpg',
+    tabletImage: '/assets/home/tablet/image-earphones-yx1.jpg',
+    mobileImage: '/assets/home/mobile/image-earphones-yx1.jpg',
+    defaultImage: '/assets/home/mobile/image-earphones-yx1.jpg',
+  };
+}
