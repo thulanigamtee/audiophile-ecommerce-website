f59ca09cda34f2602f261b8dd9fd7ed89f465059
thulanigamtee <thulanigamtee@gmail.com>
2024-12-03 08:57:23 +0200

    add details page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { HeadphonesComponent } from './pages/headphones/headphones.component';
 import { SpeakersComponent } from './pages/speakers/speakers.component';
 import { EarphonesComponent } from './pages/earphones/earphones.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 export const routes: Routes = [
   { component: HomeComponent, path: '' },
   { component: HeadphonesComponent, path: 'headphones' },
   { component: SpeakersComponent, path: 'speakers' },
   { component: EarphonesComponent, path: 'earphones' },
+  { component: DetailsComponent, path: ':category/details/:id' },
 ];
