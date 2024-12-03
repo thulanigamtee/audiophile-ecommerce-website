84f4890013db0c5ecc2f9f118e661b2512899226
thulanigamtee <thulanigamtee@gmail.com>
2024-12-03 12:24:43 +0200

    create input() for gallery item

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PicturesComponent } from '../pictures/pictures.component';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [PicturesComponent],
   templateUrl: './gallery.component.html',
 })
-export class GalleryComponent {}
+export class GalleryComponent {
+  @Input() firstItem!: {
+    desktop: string;
+    tablet: string;
+    mobile: string;
+    default: string;
+  };
+  @Input() secondItem!: {
+    desktop: string;
+    tablet: string;
+    mobile: string;
+    default: string;
+  };
+  @Input() thirdItem!: {
+    desktop: string;
+    tablet: string;
+    mobile: string;
+    default: string;
+  };
+}
