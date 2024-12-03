4586d3999b1e6653ed1e01a90d984106a069f846
thulanigamtee <thulanigamtee@gmail.com>
2024-12-03 09:32:09 +0200

    create gallery component

---
+++ src/app/components/gallery/gallery.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  imports: [],
+  templateUrl: './gallery.component.html',
+})
+export class GalleryComponent {}
