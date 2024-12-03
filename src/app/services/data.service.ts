28242b6a4bf46884cc16ee5d30fc159c99788d47
thulanigamtee <thulanigamtee@gmail.com>
2024-12-03 12:33:02 +0200

    create behavior subject for product id

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,9 +8,20 @@ import { Observable } from 'rxjs';
 export class DataService {
   private url = 'assets/data.json';
 
+  private _productId = new BehaviorSubject<string>('');
+  productId$ = this._productId.asObservable();
+
   constructor(private http: HttpClient) {}
 
   get data(): Observable<any> {
     return this.http.get<any>(`${this.url}`);
+  }
+
+  get productId() {
+    return this._productId.value;
+  }
+
+  set productId(id: string) {
+    this._productId.next(id);
   }
 }
