a3ff9d3a3fafd4c208372b64566f536e926873b3
thulanigamtee <thulanigamtee@gmail.com>
2024-12-03 10:38:09 +0200

    create other items component

---
+++ src/app/components/other-items/other-items.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other-items',
+  imports: [],
+  templateUrl: './other-items.component.html',
+})
+export class OtherItemsComponent {}
