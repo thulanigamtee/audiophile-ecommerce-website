9dcf9a40bc0ae468906b7359a109a410610fd065
thulanigamtee <thulanigamtee@gmail.com>
2024-12-03 12:26:05 +0200

    import router link

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
