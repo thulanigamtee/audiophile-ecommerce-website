4fd0ba0e622a08f479a57ed16dbb4ab6f8d66005
thulanigamtee <thulanigamtee@gmail.com>
2024-12-05 22:06:04 +0200

    add route for checkout page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HeadphonesComponent } from './pages/headphones/headphones.component';
 import { SpeakersComponent } from './pages/speakers/speakers.component';
 import { EarphonesComponent } from './pages/earphones/earphones.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
 
 export const routes: Routes = [
   { component: HomeComponent, path: '' },
@@ -11,4 +12,5 @@ export const routes: Routes = [
   { component: SpeakersComponent, path: 'speakers' },
   { component: EarphonesComponent, path: 'earphones' },
   { component: DetailsComponent, path: ':category/details/:id' },
+  { component: CheckoutComponent, path: 'checkout' },
 ];
