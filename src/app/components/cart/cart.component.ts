9aa3283e96d801aee21654550a968290de23169d
thulanigamtee <thulanigamtee@gmail.com>
2024-12-05 22:07:29 +0200

    import cart items module

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CartItem } from '../../models/cartItem.interface';
+import { Subject, takeUntil } from 'rxjs';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../button/button.component';
+import { CartItemsComponent } from '../cart-items/cart-items.component';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CommonModule, CartItemsComponent],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
 })
-export class CartComponent {
-
-}
+export class CartComponent {}
