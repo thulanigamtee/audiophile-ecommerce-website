9e350c4693a0a8d7bf129d7f5dd6adb29df8ce9f
thulanigamtee <thulanigamtee@gmail.com>
2024-12-06 21:22:42 +0200

    implement viewchild for cart component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { ProductCardComponent } from '../product-card/product-card.component';
+import { CartComponent } from '../cart/cart.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, CartComponent],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
@@ -14,4 +17,23 @@ export class NavbarComponent {
     { name: 'speakers', path: 'speakers' },
     { name: 'earphones', path: 'earphones' },
   ];
+
+  isActive: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver.observe(['(min-width:768px']).subscribe(() => {
+      if (this.isActive) this.isActive = false;
+    });
+  }
+
+  toggleMobileMenu() {
+    this.isActive = !this.isActive;
+  }
+
+  @ViewChild(CartComponent) cartComponent!: CartComponent;
+  toggleCart() {
+    this.cartComponent.toggleCart();
+  }
 }
