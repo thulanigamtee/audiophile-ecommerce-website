9a3843fcdf28c982420195f7acc40c290c28ff47
thulanigamtee <thulanigamtee@gmail.com>
2024-12-08 22:04:39 +0200

    update component

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SummaryComponent } from '../../components/checkout/summary/summary.component';
+import { FormComponent } from '../../components/checkout/form/form.component';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [FormComponent, SummaryComponent],
   templateUrl: './checkout.component.html',
 })
 export class CheckoutComponent {}
