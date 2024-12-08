459e0ac33431e4ba064477ba36a11ac46e7d1528
thulanigamtee <thulanigamtee@gmail.com>
2024-12-08 21:49:25 +0200

    create outside click directive

---
+++ src/app/directives/outside-click.directive.ts
@@ -0,0 +1,22 @@
+import {
+  Directive,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Output,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appOutsideClick]',
+})
+export class OutsideClickDirective {
+  @Output() outsideClick = new EventEmitter();
+
+  constructor(private ref: ElementRef) {}
+
+  @HostListener('document: click', ['$event.target'])
+  public onClick(target: any) {
+    const isInsideClick = this.ref.nativeElement.contains(target);
+    if (!isInsideClick) this.outsideClick.emit();
+  }
+}
