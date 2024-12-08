fbb4f95974059a0bc3344834d2f168e5ea712bab
thulanigamtee <thulanigamtee@gmail.com>
2024-12-08 22:04:07 +0200

    add functionality to increment/decrement quantity

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { DataService } from '../../services/data.service';
-import { PicturesComponent } from '../../components/pictures/pictures.component';
-import { ButtonComponent } from '../../components/button/button.component';
+import { PicturesComponent } from '../../components/shared/pictures/pictures.component';
+import { ButtonComponent } from '../../components/shared/button/button.component';
 import { CommonModule } from '@angular/common';
-import { GalleryComponent } from '../../components/gallery/gallery.component';
-import { OtherItemsComponent } from '../../components/other-items/other-items.component';
-import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { GalleryComponent } from '../../components/details/gallery/gallery.component';
+import { ProductCardComponent } from '../../components/shared/product-card/product-card.component';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../models/product.interface';
+import { OtherItemsComponent } from '../../components/details/other-items/other-items.component';
 
 @Component({
   selector: 'app-details',
@@ -23,11 +25,13 @@ import { ProductCardComponent } from '../../components/product-card/product-card
 })
 export class DetailsComponent {
   productDetails!: any;
+  quantity: number = 1;
   private destroy$ = new Subject<void>();
 
   constructor(
     private route: ActivatedRoute,
-    private dataService: DataService
+    private dataService: DataService,
+    private cartService: CartService
   ) {}
 
   ngOnInit() {
@@ -49,5 +53,17 @@ export class DetailsComponent {
         });
       },
     });
+  }
+
+  decrementQuantity() {
+    if (this.quantity !== 1) this.quantity--;
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product, this.quantity);
   }
 }
