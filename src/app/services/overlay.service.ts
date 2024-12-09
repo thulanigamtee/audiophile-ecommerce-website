26f79d9a1e0ff9b311de5297430139dc8753436a
thulanigamtee <thulanigamtee@gmail.com>
2024-12-09 20:37:53 +0200

    create overlay service

---
+++ src/app/services/overlay.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OverlayService {
+  private overlaySubject = new BehaviorSubject(false);
+  overlaySubject$ = this.overlaySubject.asObservable();
+
+  set overlay(state: boolean) {
+    this.overlaySubject.next(state);
+  }
+
+  get overlay() {
+    return this.overlaySubject.value;
+  }
+
+  constructor() {}
+}
