584af6598df8392976a716e0e50e5524f9d7af38
thulanigamtee <thulanigamtee@gmail.com>
2024-12-09 20:38:14 +0200

    import modal component

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { SummaryComponent } from '../../components/checkout/summary/summary.component';
 import { FormComponent } from '../../components/checkout/form/form.component';
+import { ModalComponent } from '../../components/checkout/modal/modal.component';
 
 @Component({
   selector: 'app-checkout',
-  imports: [FormComponent, SummaryComponent],
+  imports: [FormComponent, SummaryComponent, ModalComponent],
   templateUrl: './checkout.component.html',
 })
 export class CheckoutComponent {}
