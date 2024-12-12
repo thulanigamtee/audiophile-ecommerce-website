cfc4d9bec07f82966bbc4f5dd0964e531750b62d
thulanigamtee <thulanigamtee@gmail.com>
2024-12-12 06:11:59 +0200

    remove breakpoint observer function

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,53 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { BreakpointObserver } from '@angular/cdk/layout';
 import { CartComponent } from '../cart/cart.component';
-import { Subject, takeUntil } from 'rxjs';
 import { OverlayService } from '../../../services/overlay.service';
+import { MobileMenuComponent } from '../mobile-menu/mobile-menu.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, RouterLink, CartComponent],
+  imports: [CommonModule, RouterLink, CartComponent, MobileMenuComponent],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
-  private destroy$ = new Subject<void>();
-
   navLinks: { name: string; path: string }[] = [
-    { name: 'home', path: '' },
+    { name: 'home', path: '/' },
     { name: 'headphones', path: 'headphones' },
     { name: 'speakers', path: 'speakers' },
     { name: 'earphones', path: 'earphones' },
   ];
 
-  constructor(
-    private breakpointObserver: BreakpointObserver,
-    private overlayService: OverlayService
-  ) {}
+  constructor(private overlayService: OverlayService) {}
 
-  ngOnInit() {
-    this.breakpointObserver
-      .observe(['(min-width:768px)'])
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(() => {
-        if (this.isNavActive) this.isNavActive = false;
-      });
-  }
-
-  isNavActive: boolean = false;
+  isMobileMenuActive: boolean = false;
+  @ViewChild(MobileMenuComponent) mobileMenuComponent!: MobileMenuComponent;
   toggleMobileMenu() {
-    this.isNavActive = !this.isNavActive;
-  }
-
-  @ViewChild(CartComponent) cartComponent!: CartComponent;
-  toggleCart() {
-    this.cartComponent.toggleCart();
+    this.mobileMenuComponent.toggleMobileMenu();
     this.overlayService.overlay = !this.overlayService.overlay;
   }
 
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
+  @ViewChild(CartComponent) cartComponent!: CartComponent;
+  toggleCart(event: Event) {
+    this.cartComponent.toggleCart(event);
+    this.overlayService.overlay = !this.overlayService.overlay;
   }
 }
