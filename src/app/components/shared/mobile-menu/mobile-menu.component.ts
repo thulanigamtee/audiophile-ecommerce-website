ee91016b11fe514bf121b2563cf744878de08da4
thulanigamtee <thulanigamtee@gmail.com>
2024-12-12 06:11:24 +0200

    update component

---
+++ src/app/components/shared/mobile-menu/mobile-menu.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { OverlayService } from '../../../services/overlay.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Subject, takeUntil } from 'rxjs';
+
+@Component({
+  selector: 'app-mobile-menu',
+  imports: [RouterLink],
+  templateUrl: './mobile-menu.component.html',
+})
+export class MobileMenuComponent {
+  private destroy$ = new Subject<void>();
+  mobileMenuLinks: { image: string; name: string; path: string }[] = [
+    {
+      image: 'assets/shared/desktop/image-category-thumbnail-headphones.png',
+      name: 'headphones',
+      path: 'headphones',
+    },
+    {
+      image: 'assets/shared/desktop/image-category-thumbnail-speakers.png',
+      name: 'speakers',
+      path: 'speakers',
+    },
+    {
+      image: 'assets/shared/desktop/image-category-thumbnail-earphones.png',
+      name: 'earphones',
+      path: 'earphones',
+    },
+  ];
+
+  constructor(
+    private router: Router,
+    private breakpointObserver: BreakpointObserver,
+    private overlayService: OverlayService
+  ) {}
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(['(min-width:1024px)'])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        if (this.isMobileMenuActive) {
+          this.isMobileMenuActive = false;
+          this.overlayService.overlay = false;
+        }
+      });
+  }
+
+  isMobileMenuActive: boolean = false;
+  toggleMobileMenu() {
+    this.isMobileMenuActive = !this.isMobileMenuActive;
+  }
+
+  goToPage(page: string) {
+    this.router.navigate([page]);
+  }
+}
