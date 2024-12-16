0c5cbc954182e7df6edd1f2ca01e96442def04eb
thulanigamtee <thulanigamtee@gmail.com>
2024-12-16 12:22:50 +0200

    remove bootstrap defer

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-function waitForStyles() {
-  return new Promise<void>((resolve) => {
-    const stylesheets = Array.from(document.styleSheets).filter((sheet) =>
-      sheet.href?.includes('styles.css')
-    );
-
-    const checkLoaded = () => {
-      if (stylesheets.every((sheet) => sheet.cssRules?.length > 0)) {
-        resolve();
-      } else {
-        requestAnimationFrame(checkLoaded);
-      }
-    };
-
-    checkLoaded();
-  });
-}
-
-waitForStyles().then(() => {
-  bootstrapApplication(AppComponent, appConfig).catch((err) =>
-    console.error(err)
-  );
-});
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
