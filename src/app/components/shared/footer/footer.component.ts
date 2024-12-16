bf286e6899953dad9f397ec4e6d0f749a03851af
thulanigamtee <thulanigamtee@gmail.com>
2024-12-16 10:35:01 +0200

    implement router link

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
