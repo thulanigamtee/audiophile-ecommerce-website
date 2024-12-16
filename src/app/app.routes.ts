8320989b85f7a9d6e102d6ba225bd80796c27010
thulanigamtee <thulanigamtee@gmail.com>
2024-12-16 12:28:47 +0200

    remove lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { HeadphonesComponent } from './pages/headphones/headphones.component';
+import { SpeakersComponent } from './pages/speakers/speakers.component';
+import { EarphonesComponent } from './pages/earphones/earphones.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
 
 export const routes: Routes = [
   {
@@ -12,37 +17,42 @@ export const routes: Routes = [
   },
   {
     path: 'headphones',
-    loadComponent: () =>
-      import('./pages/headphones/headphones.component').then(
-        (component) => component.HeadphonesComponent
-      ),
+    // loadComponent: () =>
+    //   import('./pages/headphones/headphones.component').then(
+    //     (component) => component.HeadphonesComponent
+    //   ),
+    component: HeadphonesComponent,
   },
   {
     path: 'speakers',
-    loadComponent: () =>
-      import('./pages/speakers/speakers.component').then(
-        (component) => component.SpeakersComponent
-      ),
+    // loadComponent: () =>
+    //   import('./pages/speakers/speakers.component').then(
+    //     (component) => component.SpeakersComponent
+    //   ),
+    component: SpeakersComponent,
   },
   {
     path: 'earphones',
-    loadComponent: () =>
-      import('./pages/earphones/earphones.component').then(
-        (component) => component.EarphonesComponent
-      ),
+    // loadComponent: () =>
+    //   import('./pages/earphones/earphones.component').then(
+    //     (component) => component.EarphonesComponent
+    //   ),
+    component: EarphonesComponent,
   },
   {
     path: ':category/details/:id',
-    loadComponent: () =>
-      import('./pages/details/details.component').then(
-        (component) => component.DetailsComponent
-      ),
+    // loadComponent: () =>
+    //   import('./pages/details/details.component').then(
+    //     (component) => component.DetailsComponent
+    //   ),
+    component: DetailsComponent,
   },
   {
     path: 'checkout',
-    loadComponent: () =>
-      import('./pages/checkout/checkout.component').then(
-        (component) => component.CheckoutComponent
-      ),
+    // loadComponent: () =>
+    //   import('./pages/checkout/checkout.component').then(
+    //     (component) => component.CheckoutComponent
+    //   ),
+    component: CheckoutComponent,
   },
 ];
