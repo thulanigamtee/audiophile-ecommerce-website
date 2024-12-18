d0260bc66ac4a19910fef30219eddfd9e1029dc1
thulanigamtee <thulanigamtee@gmail.com>
2024-12-18 12:22:21 +0200

    remove unnecessary code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,50 +9,26 @@ import { CheckoutComponent } from './pages/checkout/checkout.component';
 export const routes: Routes = [
   {
     path: '',
-    // loadComponent: () =>
-    //   import('./pages/home/home.component').then(
-    //     (component) => component.HomeComponent
-    //   ),
     component: HomeComponent,
   },
   {
     path: 'headphones',
-    // loadComponent: () =>
-    //   import('./pages/headphones/headphones.component').then(
-    //     (component) => component.HeadphonesComponent
-    //   ),
     component: HeadphonesComponent,
   },
   {
     path: 'speakers',
-    // loadComponent: () =>
-    //   import('./pages/speakers/speakers.component').then(
-    //     (component) => component.SpeakersComponent
-    //   ),
     component: SpeakersComponent,
   },
   {
     path: 'earphones',
-    // loadComponent: () =>
-    //   import('./pages/earphones/earphones.component').then(
-    //     (component) => component.EarphonesComponent
-    //   ),
     component: EarphonesComponent,
   },
   {
     path: ':category/details/:id',
-    // loadComponent: () =>
-    //   import('./pages/details/details.component').then(
-    //     (component) => component.DetailsComponent
-    //   ),
     component: DetailsComponent,
   },
   {
     path: 'checkout',
-    // loadComponent: () =>
-    //   import('./pages/checkout/checkout.component').then(
-    //     (component) => component.CheckoutComponent
-    //   ),
     component: CheckoutComponent,
   },
 ];
